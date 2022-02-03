430fa1da393a91398d596c05ba1ade9584bc7d28
bellaking0 <77692024+bellaking0@users.noreply.github.com>
2022-02-03 17:53:01 -0600

    commit to 'main'

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var firstfortune = "Remember, that bad times... are just times that are bad"
+let firstfortune = "May happiness find you and yours... Remember, that bad times... are just times that are bad";
+let secondfortune = "I can deduce that your luck with money and belongings will reduce";
+let length = txt.length;
+let part = str.slice(firstfortune)
 function sayFortune() {
-    window.alert(firstfortune)
+    alert(firstfortune);
 }
